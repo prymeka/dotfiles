51daeecf59c6cfcff400bea95200c1d13b79bfb0
Aleksander <prymeka@tcd.ie>
2023-04-13 17:30:28 +0100

    updated navigation keymaps

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -24,6 +24,11 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+-- Navigation
+keymap("n", "<C-u>", "<C-u>zz", opts)
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "n", "nzz", opts)
+keymap("n", "N", "Nzz", opts)
 -- Explorer
 keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 -- Find and replace
